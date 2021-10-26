72abd80a4524aa06fe24233a824266f0fdff1689
Ítallo Márlon <itallo1000724@gmail.com>
2021-10-26 15:46:29 -0300

    :pencil: Atualizando Arquivo

--- main.js
+++ main.js
@@ -6,11 +6,24 @@ const linksSocialMedias = {
   twitter: ""
 }
 
-function getGitHubUsersInfo(){
+function changeSocialMedialLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedias[social]}`
+  }
+}
+
+function getGitHubUsersInfo() {
   const url = `https://api.github.com/users/${linksSocialMedias.github}`
-  
+
   fetch(url).then(response => response.json())
-  .then(data => {
-  })
+    .then(data => {
+      myName.textContent = data.name
+      aboutMe.textContent = data.bio
+      login.textContent = data.login
+      avatar.src = data.avatar_url
+
+    })
 }
 getGitHubUsersInfo()
+changeSocialMedialLinks()
